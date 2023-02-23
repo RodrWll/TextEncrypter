fc605df5ac40d4c95a2ae2a358717f8594c5ade6
Rodrigo <a20202021@pucp.edu.pe>
2023-02-23 11:38:39 -0500

    Cambio de nombre del archivo js y creación de variables

---
+++ script.js
@@ -0,0 +1,17 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+
+// Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+function encrypt()
+{
+    let matrizCodigo = [["e", "enter"], ["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    
+
+}
